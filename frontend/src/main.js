dee5a185eac99efeb48fe6ce411cd8d3b1195aa0
Alex Walker <alex.walker@whiteops.com>
2025-06-07 14:24:15 +0100

    sorting icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,41 @@
 "use strict";
+import hotspotIcon from './assets/images/cell_tower.svg';
+import wifiIcon from './assets/images/wifi.svg';
 
+const icon = document.getElementById("wifi-mode-icon");
+
+// Mirror your Go enum values:
+const MODE_UNKNOWN = 0;
+const MODE_INFRA   = 1;
+const MODE_AP      = 2;
+
+icon.src = wifiIcon
+window.runtime.EventsOn("jetclock:wifi.mode", (mode) => {
+    console.log("📶 wifi.mode event:", mode, typeof mode);
+    switch (mode) {
+        case MODE_AP:
+            console.log("mode_ap ", hotspotIcon)
+            icon.src = hotspotIcon;
+            icon.alt = "Hotspot";
+            break;
+
+        case MODE_INFRA:
+            console.log("mode_infra ", hotspotIcon)
+            icon.src = wifiIcon;
+            icon.alt = "Wi-Fi";
+            break;
+
+        default:
+            console.log("mode_unknown ", hotspotIcon)
+            // Fallback (unknown)
+            icon.src = wifiIcon;
+            icon.alt = "Unknown";
+            break;
+    }
+});
+
+
+//clock:
 const DEGREES_PER_HOUR = 360 / 12;
 const DEGREES_PER_MINUTE = 360 / 60;
 const DEGREES_PER_SECOND = 360 / 60;
