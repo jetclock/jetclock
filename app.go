69cb198f3bd5f2b17f92d4f7b473a92831e69909
oliverrees <5574826+oliverrees@users.noreply.github.com>
2026-01-02 20:43:51 +0000

    Change package install ordering

--- app.go
+++ app.go
@@ -63,12 +63,15 @@ func (a *App) domReady(ctx context.Context) {
 
 	logger.Log.Infof("[%s] DOM ready complete, splash screen should close\n", time.Now().Format("2006-01-02 15:04:05"))
 
-	// Initialize GPIO environment after splash screen is cleared and device is ready
-	logger.Log.Infof("[%s] Starting GPIO initialization now that app is ready\n", time.Now().Format("2006-01-02 15:04:05"))
-	if err := utils.InitializeGPIOEnvironment(); err != nil {
-		logger.Log.Errorf("GPIO initialization failed: %v", err)
-		// Continue anyway, app can still function without GPIO
-	}
+	// Initialize GPIO environment in background after splash screen is cleared
+	go func() {
+		logger.Log.Infof("[%s] Starting background GPIO initialization now that app is ready\n", time.Now().Format("2006-01-02 15:04:05"))
+		if err := utils.InitializeGPIOEnvironment(); err != nil {
+			logger.Log.Errorf("Background GPIO initialization failed: %v", err)
+			// Continue anyway, app can still function without GPIO
+		}
+		logger.Log.Infof("[%s] Background GPIO initialization completed\n", time.Now().Format("2006-01-02 15:04:05"))
+	}()
 }
 
 func (a *App) GetSystemID() string {
